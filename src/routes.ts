29f31d91a22e9058571e75251b5179425e84a0f6
mamonov <mmvvlad@gmail.com>
2022-06-22 12:42:43 +0700

    problem with handlers, somehow js start asking for using imports, but here no require functions. Something strange

--- src/routes.ts
+++ src/routes.ts
@@ -5,13 +5,6 @@ import {createUserSchema} from "./schema/user.schema";
 import {createUserSessionHandler, deleteSessionHandler, getUserSessionsHandler} from "./controller/session.controller";
 import {createSessionSchema} from "./schema/session.schema";
 import requireUser from "./middleware/requireUser";
-import {createProductSchema, deleteProductSchema, getProductSchema, updateProductSchema} from "./schema/product.schema";
-import {
-    createProductHandler,
-    deleteProductHandler,
-    getProductHandler,
-    updateProductHandler
-} from "./controller/product.controller";
 
 function routes(app: Express) {
     app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
@@ -22,10 +15,10 @@ function routes(app: Express) {
     app.get('/api/sessions', requireUser, getUserSessionsHandler)
     app.delete('/api/sessions', requireUser, deleteSessionHandler)
 
-    app.post('/api/products', [requireUser, validateResource(createProductSchema)], createProductHandler)
-    app.put('/api/products/:productId', [requireUser, validateResource(updateProductSchema)], updateProductHandler)
-    app.get('/api/products/:productId', validateResource(getProductSchema), getProductHandler)
-    app.delete('/api/products/:productId', [requireUser, validateResource(deleteProductSchema)], deleteProductHandler)
+    // app.post('/api/products', [requireUser, validateResource(createProductSchema)], createProductHandler)
+    // app.put('/api/products/:productId', [requireUser, validateResource(updateProductSchema)], updateProductHandler)
+    // app.get('/api/products/:productId', validateResource(getProductSchema), getProductHandler)
+    // app.delete('/api/products/:productId', [requireUser, validateResource(deleteProductSchema)], deleteProductHandler)
 }
 
 export default routes
